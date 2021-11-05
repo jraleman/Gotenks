3935578e43ab6e0c665f073471b1fbebe9c96eb6
jraleman <jraleman@bendiburg.com>
2021-11-04 19:26:08 -0700

    Setups Cordova + CRA project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './themes/index.css';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './config/serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
+const renderReactDom = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
 
-const startApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+  renderReactDom();
 }
 
-if (!window.cordova) {
-  startApp()
-}
-else {
-  document.addEventListener('deviceready', startApp, false)
-}
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
